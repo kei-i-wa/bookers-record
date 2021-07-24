86d684081ad85d2a7babb215ca1bf4f66be484a7
kei-i-wa <kei.kumarg@gmail.com>
2021-07-24 13:48:00 +0000

    ADD 今日と昨日の投稿数

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+ before_action :authenticate_user!
+ before_action :correct_user, only: [:edit, :update]
+
+
+
+  def show
+    @user=User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+    from=Time.current.at_beginning_of_day
+    to=Time.current.at_end_of_day
+    @book_a_day=@books.where(created_at: from...to)
+    @book_a_yesterday=@books.where(created_at: 1.day.ago.all_day)
+
+    @book=Book.new
+  end
+
+  def index
+   @users=User.page(params[:page]).reverse_order
+   @book=Book.new
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to user_path(@user.id),notice:'You have updated user successfully.'
+    else
+      render:edit
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_url(current_user)) unless @user == current_user
+  end
+
+  
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image,:introduction)
+  end
+
+end
