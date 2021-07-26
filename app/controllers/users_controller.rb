f4894b410b6170484aa1d7b6a3824a15057e4868
kei-i-wa <kei.kumarg@gmail.com>
2021-07-26 00:47:16 +0000

    UPDATE折線ぐらふ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,25 +7,10 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
     @books = @user.books.page(params[:page]).reverse_order
-    # from=Time.current.at_beginning_of_day
-    # to=Time.current.at_end_of_day
-    # @book_a_day=@books.where(created_at: from...to)
-    # @book_a_yesterday=@books.where(created_at: 1.day.ago.all_day)
-    # @book_a_week=@books.where(created_at: 6.days.ago...to)
-    # @book_a_lastweek=@books.where(created_at: 14.days.ago...to-7.days)
     @today_book =  @books.created_today
     @yesterday_book = @books.created_yesterday
-    # @twodays_book = @books.created_2days
-    # @threedays_book = @books.created_3days
-    # @fourdays_book = @books.created_4days
-    # @fivedays_book = @books.created_5days
-    # @sixdays_book = @books.created_6days
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
-    # @books_count = Book.group_by_day(:created_at).size
-    # @book_by_day = @books.group_by_day(:created_at).size
-    # @chartlabels = @book_by_day.map(&:first).to_json.html_safe
-    # @chartdatas = @book_by_day.map(&:second)
     @book=Book.new
   end
 
